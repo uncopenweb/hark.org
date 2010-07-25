4d158c9a9aba90eb297e8054a7d96637fe1be415
Peter Parente <parente@cs.unc.edu>
2010-07-24 22:22:00 -0400

    Basic next/prev results nav

--- widgets/ThumbnailView.js
+++ widgets/ThumbnailView.js
@@ -13,39 +13,59 @@ dojo.require('dojo.i18n');
 dojo.requireLocalization('org.hark', 'ThumbnailView');
 
 dojo.declare('org.hark.ThumbnailView', [dijit._Widget, dijit._Templated], {
+    // data mode
     model: null,
+    // results per row
+    rowSize: 6,
     widgetsInTemplate: true,
     templatePath: dojo.moduleUrl('org.hark.templates', 'ThumbnailView.html'),
 
     postMixInProperties: function() {
-        // current page
-        this._currentPage = 0;
         // query template
         this._queryTemplate = 'label: *{text}* OR description: *{text}*';
         // current results on page
-        this._results = 0;
+        this._shownCount = 0;
+        // current page
+        this._page = 0;
         this.labels = dojo.i18n.getLocalization('org.hark', 'ThumbnailView');
-        this.subscribe('/search', 'showPage');
+        this.subscribe('/search', 'setQuery');
     },
     
     postCreate: function() {
-        // show the first set of results
-        //this.showPage();
     },
     
     resize: function(box) {
         this.containerNode.resize(box);
     },
     
-    showPage: function(text) {
+    setQuery: function(text) {
+        this._query = text;
+        this._search();
+    },
+    
+    _onNextPage: function() {
+        console.log('_onNextPage');
+        this._page += 1;
+        this._search();
+    },
+    
+    _onPrevPage: function() {
+        this._page -= 1;
+        this._search();        
+    },
+    
+    _search: function() {
         // build the query
-        var q = dojo.replace(this._queryTemplate, {text : text});
+        var q = dojo.replace(this._queryTemplate, {text : this._query});
         var req = this.model.fetch({
             query: {complexQuery : q},
             onBegin: this._onBegin,
             onItem: this._onItem,
             onComplete: this._onComplete,
             onError: this._onError,
+            start: this._page * this.rowSize,
+            // fetch one extra to check if there's a next
+            count: this.rowSize + 1,
             queryOptions: {
                 deep: false,
                 ignoreCase: true
@@ -61,33 +81,39 @@ dojo.declare('org.hark.ThumbnailView', [dijit._Widget, dijit._Templated], {
     _onBegin: function(size, request) {
         // clear the table cells
         dojo.query('td', this.tableNode).forEach('dojo.destroy(item);');
+        // reset the count
+        this._shownCount = 0;
         // @todo: show message if no results
     },
     
     _onItem: function(item) {
         var row = this.row0;
-        if(this._results >= 3) {
+        if(this._shownCount >= this.rowSize / 2.0) {
             row = this.row1;
         }
-        var td = dojo.create('td', null, row);
-        if(item) {
-            var img = dojo.create('img', {
-                src : this.model.getValue(item, 'media').icon
-            }, td);
-            dojo.create('br', null, td);
-            var span = dojo.create('a', {
-                href : '#'+this.model.getValue(item, 'hash'),
-                innerHTML : this.model.getValue(item, 'label')
-            }, td);
+        if(this._shownCount < this.rowSize) {
+            var td = dojo.create('td', null, row);
+            if(item) {
+                var img = dojo.create('img', {
+                    src : this.model.getValue(item, 'media').icon
+                }, td);
+                dojo.create('br', null, td);
+                var span = dojo.create('a', {
+                    href : '#'+this.model.getValue(item, 'hash'),
+                    innerHTML : this.model.getValue(item, 'label')
+                }, td);
+            }
         }
-        this._results += 1;
+        this._shownCount += 1;
     },
     
     _onComplete: function() {
-        while(this._results < 6) {
+        while(this._shownCount < this.rowSize) {
             // fill blank cells
             this._onItem(null);
         }
+        this.nextButton.attr('disabled', this._shownCount <= this.rowSize);
+        this.prevButton.attr('disabled', this._page == 0);
     },
     
     _onError: function(err) {
